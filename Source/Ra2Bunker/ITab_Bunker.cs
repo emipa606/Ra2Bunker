4953c6eda564796cd9083821d23c60fd4a657c2d
Mlie <emipa606@users.noreply.github.com>
2022-04-16 20:42:02 +0200

    Added Pawn-button to inspect pawns inside of the bunker

---
+++ Source/Ra2Bunker/ITab_Bunker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Ra2Bunker;
+
+public class ITab_Bunker : ITab_ContentsBase
+{
+    private readonly List<Thing> listInt = new List<Thing>();
+
+    public ITab_Bunker()
+    {
+        labelKey = "TabBunkerContents";
+        containedItemsKey = "ContainedItems";
+        canRemoveThings = false;
+    }
+
+    public override IList<Thing> container
+    {
+        get
+        {
+            var building_Casket = SelThing as Building_Bunker;
+            listInt.Clear();
+
+            if (building_Casket is not { HasAnyContents: true })
+            {
+                return listInt;
+            }
+
+            foreach (var pawn in building_Casket.GetInner())
+            {
+                listInt.Add(pawn);
+            }
+
+            return listInt;
+        }
+    }
+}
